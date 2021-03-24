5f5f4ea7ba322abc00877d01ee2e29b117e9ac44
GiuliaRosignoli <giuliarosignoli@gmail.com>
2021-03-24 15:55:36 +0100

    .

--- Script/script.js
+++ Script/script.js
@@ -16,3 +16,35 @@ var lastNamesList = ["bianchi", "neri","verdi", "rossi", "gialli"];
 lastNamesList.push(lastName);
 lastNamesList.sort();
 console.log(lastNamesList);
+
+
+// 2nd part Write down the correct index position
+
+
+function array_move(lastNamesList, [0], [1]) {
+    if(1 >= lastNamesList.length) {
+        var k = [1] - [0] + 1;
+
+        while (k--) {
+            lastNamesList.push(undefined);
+        }
+    }
+    lastNamesList.splice([1], 0, lastNamesList([0], 1)[0];
+    return lastNamesList;
+}
+
+
+
+/* function array_move(arr, old_index, new_index) {
+    if (new_index >= arr.length) {
+        var k = new_index - arr.length + 1;
+        while (k--) {
+            arr.push(undefined);
+        }
+    }
+    arr.splice(new_index, 0, arr.splice(old_index, 1)[0]);
+    return arr; // for testing
+};
+
+// returns [2, 1, 3]
+console.log(array_move([1, 2, 3], 0, 1));  /*
